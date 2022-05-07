59ff8e3bb94d49cca7299c78939a63ec71985a65
shimu21ph <shimu21ph@gmail.com>
2022-05-08 03:17:35 +0600

    api implemented

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-
+const { ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
@@ -21,6 +21,60 @@ async function run() {
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service)
+        })
+
+
+        app.post('/services', async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            res.send(result);
+        })
+
+        app.put('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    price: updatedUser.price,
+                    quantity: updatedUser.quantity,
+                    img: updatedUser.img,
+                    description: updatedUser.description
+                }
+            };
+            const result = await servicesCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+        app.put('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            console.log(updatedUser);
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedUser.quantity
+                }
+            };
+            const result = await servicesCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await servicesCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {
