fb60f5879469c108d30a44dadbff1ed43a4803c7
shimu21ph <shimu21ph@gmail.com>
2022-05-08 23:05:27 +0600

    nodemon added

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const { ObjectId } = require('mongodb');
+
 app.use(cors());
 app.use(express.json());
 
